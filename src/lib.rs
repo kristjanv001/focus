efa9b6ef77441efabbfc0f10d0511bf5d3f6f96e
Kristjan Vingel <kristjan.vingel@protonmail.ch>
2024-12-26 23:38:04 +0200

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+// pub struct Config {}
+
+pub fn run() {
+  println!("focusing...");
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn mytest() {}
+}
+
+// show text focusing...
+// quit when user types 'q'
