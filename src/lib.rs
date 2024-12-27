77721ee5046ced50fb1e200d5608995fa857e5fa
Kristjan Vingel <kristjan.vingel@protonmail.ch>
2024-12-27 02:30:12 +0200

    Hide cursor

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-use crossterm::event;
-use crossterm::terminal;
+use crossterm::{event, terminal, ExecutableCommand, cursor};
 use std::error;
+use std::io;
 
 // pub struct Config {}
 struct CleanUp;
@@ -14,13 +14,16 @@ impl Drop for CleanUp {
 pub fn run() -> Result<(), Box<dyn error::Error>> {
   let _cleanup = CleanUp;
 
-  println!("focusing...");
+  println!("🍅 focusing...");
   // io::stdout().flush()?;
 
   terminal::enable_raw_mode().map_err(|e| {
     eprintln!("Failed to enable raw mode: {}", e);
     e
   })?;
+
+  io::stdout().execute(cursor::Hide).expect("Could not disable disable blinking");
+
 
   loop {
     if let event::Event::Key(event) = event::read().unwrap() {
